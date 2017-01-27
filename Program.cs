e245f5bd3502b82d8081e8c12921e05ebd947311
Andrey <metalhead190113@gmail.com>
2017-01-27 13:48:18 +0600

    First and one commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringsTest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int num;
+			int nextdigit;
+			int numdigits;
+			int[] n = new int[20];
+			string[] digits = { "нуль", "один", "два",
+					   "три", "четыре", "пять",
+					   "шесть", "семь", "восемь",
+					   "девять" };
+			num = 1908;
+			Console.WriteLine("Число: " + num);
+			Console.Write("Число словами: ");
+			nextdigit = 0;
+			numdigits = 0;
+// Получить отдельные цифры и сохранить их в массиве n.
+// Эти цифры сохраняются в обратном порядке.
+			do
+			{
+				nextdigit = num % 10;
+				n[numdigits] = nextdigit;
+				numdigits++;
+				num = num / 10;
+			} while (num > 0);
+			numdigits--;
+			// Вывести полученные слова.
+			for (; numdigits >= 0; numdigits--)
+				Console.Write(digits[n[numdigits]] + " ");
+			Console.WriteLine();
+		}
+	}
+}
